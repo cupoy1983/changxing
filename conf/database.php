455f857268c3300ee04dae71ac72146bb01c3c2a
frankie <cupoy1983@gmail.com>
2013-09-27 18:47:06 +0800

    first check in.

---
+++ conf/database.php
@@ -0,0 +1,10 @@
+<?php
+  return array(
+	'dsn' => 'mysql:host=localhost;dbname=changxing;port=3306',
+	'user' => 'fairy',
+	'pwd' => 'wyff1688fairy',
+	'charset' => 'utf8',
+	'tableprefix' => 'bmw_',
+	'engine' => 'MyISAM',
+);
+?>
---
+++ conf/founder.php
@@ -0,0 +1,5 @@
+<?php
+  return array(
+	'admin' => '7eabd624e42533dfead4bc1fb28255b5',
+);
+?>
